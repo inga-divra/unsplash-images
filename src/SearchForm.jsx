932900ee49447634533b237778e539a68d6d2c6d
inga-divra <ingady@gmail.com>
2024-07-22 15:42:21 +0300

    Created SearchForm structure with input and submit functionality
    - Created a form with an input and a submit button.
    - Set input attributes: type='text', name='search', placeholder='cat', and className='form-input search-input'.
    - Implemented form submission to log the input value (handleSubmit)

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,8 +1,31 @@
+import { useGlobalContext } from './context'
 
 const SearchForm = () => {
-    return (
-        <div>SearchForm</div>
-    )
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const searchValue = e.target.search.value
+        if (!searchValue) return
+        console.log(searchValue);
+    }
+
+    return <section>
+        <h1 className='title'>unsplush images</h1>
+        <form
+            className='search-form'
+            onSubmit={handleSubmit}>
+            <input
+                className='form-input'
+                type='text'
+                name='search'
+                placeholder='cat' />
+            <button
+                className='btn'
+                type='submit'>
+                search
+            </button>
+        </form>
+    </section>
 }
 
 export default SearchForm
