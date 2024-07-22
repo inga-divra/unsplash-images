ad07f7e2c49cbc079bd13448cc9ca6f6598506b8
inga-divra <ingady@gmail.com>
2024-07-22 20:04:52 +0300

    Implemented initial dark mode detection and applied theme based on user preference
    - Used getInitialDarkMode to check if the user prefers dark mode;
    
    - Added useEffect to apply the dark/light theme based on isDarkTheme

--- src/context.jsx
+++ src/context.jsx
@@ -2,17 +2,27 @@ import { useState, useEffect, createContext, useContext } from "react";
 
 const AppContext = createContext()
 
+// Dark Mode - JS: Check which theme the user prefers in their browser
+const getInitialDarkMode = () => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    console.log(prefersDarkMode);
+    return prefersDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(false)
+    const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
     const [searchTerm, setSearchTerm] = useState('racoon')
 
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme
         setIsDarkTheme(newDarkTheme)
-        const body = document.querySelector('body')
-        body.classList.toggle('dark-theme', newDarkTheme)
-        console.log(body);
     }
+
+    // Sets dark/light theme according to user's preference when the app loads
+    useEffect(() => {
+        document.body.classList.toggle('dark-theme', isDarkTheme)
+    }, [isDarkTheme])
+
     return <AppContext.Provider
         value={{
             isDarkTheme,
